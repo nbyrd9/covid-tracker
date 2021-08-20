6697f3116b615c57b7bbc3e7083050868e94039e
nbyrd9 <nianibyrd@gmail.com>
2021-08-20 13:02:00 -0400

    create 4 cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,3 +3,11 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import CountUp from "react-countup";
 import cx from "classnames";
+
+const Cards = ({
+    data: { confirmed, recovered, deaths, lastUpdate },
+    country,
+  }) => {
+    if (!confirmed) {
+      return "Loading...";
+    }
