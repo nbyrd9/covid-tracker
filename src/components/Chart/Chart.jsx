6627024fa212f8320a381dd422447afecf7446ef
nbyrd9 <nianibyrd@gmail.com>
2021-09-29 10:21:54 -0400

    chart variable

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import { fetchDailyData } from "../../api"
 import { Line, Bar } from "react-chartjs-2"
 import styles from "./Chart.module.css"
 
-
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 
 
 export default Chart
