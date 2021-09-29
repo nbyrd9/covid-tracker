2d963c360adc928428f8ddd22bdd1d2eb4785410
nbyrd9 <nianibyrd@gmail.com>
2021-09-29 10:26:27 -0400

    start labels

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -12,4 +12,6 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
         fetchAPI()
     }, [])
     const lineChart = dailyData.length ? (
+        <Line
+      data={{
 export default Chart
