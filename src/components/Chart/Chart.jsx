388df1c0ae4bd7600b2b63d90c550c4267606612
nbyrd9 <nianibyrd@gmail.com>
2021-09-28 20:38:28 -0400

    update code

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { fetchDailyData } from "../../api";
-import { Line, Bar } from "react-chartjs-2";
-import styles from "./Chart.module.css";
+import React, { useState, useEffect } from "react"
+import { fetchDailyData } from "../../api"
+import { Line, Bar } from "react-chartjs-2"
+import styles from "./Chart.module.css"
 
 
 
 
-export default Chart;
+export default Chart
