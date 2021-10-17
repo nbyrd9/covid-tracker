393db9da68d49bb311e46982200ed28e6557477f
nbyrd9 <nianibyrd@gmail.com>
2021-10-17 15:05:45 -0400

    debug

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -35,7 +35,12 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   ) : null;
 
   const barChart = confirmed ? (
-  
+    <Bar
+    data={{
+      labels: ["Infected", "Recovered", "Deaths"],
+      datasets: [
+        {
+          label: "People",
 
 
 
