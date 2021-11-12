55650dab93a9ae3ebe6316a1bc2b85c87de8196e
nbyrd9 <nianibyrd@gmail.com>
2021-11-12 10:39:40 -0500

    fetch data

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,3 +1,4 @@
 import React, { useState, useEffect } from "react";
 import { NativeSelect, FormControl } from "@material-ui/core";
 import styles from "./CountryPicker.module.css";
+import { fetchCountries } from "../../api";
