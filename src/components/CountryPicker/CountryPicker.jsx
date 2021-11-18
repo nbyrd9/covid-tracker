d1c82e5e314e8f58e64292192526441c0a6187e0
nbyrd9 <nianibyrd@gmail.com>
2021-11-18 17:18:05 -0500

    fetch countries

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -19,3 +19,4 @@ const CountryPicker = ({ handleCountryChange }) => {
             onChange={(e) => handleCountryChange(e.target.value)}
             >
                 <option value="">Global</option>
+                {fetchedCountries.map((country, key) => (
