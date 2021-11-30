502bcb16797aea7c3b1675f57f6092886571e872
nbyrd9 <nianibyrd@gmail.com>
2021-11-30 09:46:25 -0500

    restart

--- src/App.js
+++ src/App.js
@@ -16,4 +16,6 @@ class App extends React.Component {
     }
 
     handleCountryChange = async (country) => {
-       
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+      };
