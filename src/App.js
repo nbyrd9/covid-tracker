eb02f10102ab74cf9b4a27e229ce22909bfb7ba5
nbyrd9 <nianibyrd@gmail.com>
2021-12-08 15:52:02 -0500

    render method

--- src/App.js
+++ src/App.js
@@ -19,3 +19,6 @@ class App extends React.Component {
         const fetchedData = await fetchData(country);
         this.setState({ data: fetchedData, country: country });
       };
+
+
+      render() {
